073a1d90f8c9782db6c989496885c49be02e91ed
jackplugged <sidneisimmon@gmail.com>
2018-08-05 11:23:42 -0300

    Solução desafio ano bissexto

---
+++ ano-bissexto/index.js
@@ -0,0 +1,16 @@
+const rest = (a, b) => (a % b);
+
+const isLeapYear = year => {
+    const restFour = rest(year, 4);
+    const restHundred = rest(year, 100);
+    const restFourHundred = rest(year, 400);
+
+    if ((!restFour) &&
+        (restHundred || !restHundred && !restFourHundred)) {
+        return true;
+    }
+    return false;
+}
+
+const year = 2022;
+isLeapYear(year);
